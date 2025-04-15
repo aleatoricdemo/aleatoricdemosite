ca1d122f135f2264053f6d5fff2b05b1f261325e
aleatoricdemo <zsolt.nadas@aleatoricsolutions.com>
2025-04-15 15:47:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.log('JavaScript is running');
 document.addEventListener('DOMContentLoaded', function() {
     document.body.style.backgroundColor = 'lightblue'; // Change background color
     console.log('JavaScript is running');
